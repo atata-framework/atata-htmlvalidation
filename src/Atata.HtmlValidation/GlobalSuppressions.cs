06312fe48e9829adb1db6229dbe10e0441f960da
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2025-06-22 13:48:44 +0200

    Update GlobalSuppressions.cs

--- src/Atata.HtmlValidation/GlobalSuppressions.cs
+++ src/Atata.HtmlValidation/GlobalSuppressions.cs
@@ -1,8 +1,4 @@
-﻿using System.Diagnostics.CodeAnalysis;
-
-#pragma warning disable S103 // Lines should not be too long
+﻿#pragma warning disable S103 // Lines should not be too long
 
 [assembly: SuppressMessage("Critical Code Smell", "S2696:Instance members should not write to \"static\" fields", Justification = "<Pending>", Scope = "member", Target = "~M:Atata.HtmlValidation.HtmlValidator.EnsureCliIsInstalled")]
 [assembly: SuppressMessage("Style", "IDE0053:Use expression body for lambda expressions", Justification = "<Pending>", Scope = "member", Target = "~M:Atata.PageObjectHtmlValidateExtensions.ValidateHtml``1(``0,Atata.HtmlValidation.HtmlValidationOptions,System.Boolean)~``0")]
-
-#pragma warning restore S103 // Lines should not be too long
