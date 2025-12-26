d0920484c01d0ac8076ceb3359bb9657d69b790f
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2025-12-26 16:46:11 +0100

    Update `Guard`

--- src/Atata.HtmlValidation/GlobalUsings.cs
+++ src/Atata.HtmlValidation/GlobalUsings.cs
@@ -1,4 +1,5 @@
 ﻿global using System.ComponentModel;
+global using System.Diagnostics;
 global using System.Diagnostics.CodeAnalysis;
 global using System.Runtime.CompilerServices;
 global using System.Text;
